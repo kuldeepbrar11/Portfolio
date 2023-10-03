6a12348e83a99fd5d011836676cd84f7568c5be7
kuldeepbrar11 <brarkuldeep555@gmail.com>
2023-10-03 02:44:44 -0400

    Made couple of changes.

--- index.js
+++ index.js
@@ -5,41 +5,16 @@ const port = 2000;
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 
-// Define routes
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/projects', (req, res) => {
-  res.render('projects');
-});
-
-app.get('/services', (req, res) => {
-  res.render('services');
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-// Set the view engine to EJS
-app.set('view engine', 'ejs');
-
 // Import routes
-const routes = require('./routes');
+const routes = require('./routes/index.js');
 
 // Use routes
 app.use('/', routes);
 app.use(express.static('public', { 'extensions': ['html', 'css'] }));
+
+app.use(expressLayouts);
+app.set('header', 'views/partials/header.ejs'); // Define the path to your layout file
+
 
 // Start the server
 app.listen(port, () => {
