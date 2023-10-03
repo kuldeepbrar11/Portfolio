db537158d87d6e29f92639720cb6af4efe0657ea
kuldeepbrar11 <brarkuldeep555@gmail.com>
2023-10-03 03:16:52 -0400

    abcd1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 2000;
+const port = 2001;
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -11,10 +11,6 @@ const routes = require('./routes/index.js');
 // Use routes
 app.use('/', routes);
 app.use(express.static('public', { 'extensions': ['html', 'css'] }));
-
-app.use(expressLayouts);
-app.set('header', 'views/partials/header.ejs'); // Define the path to your layout file
-
 
 // Start the server
 app.listen(port, () => {
